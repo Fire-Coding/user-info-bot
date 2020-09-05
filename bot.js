e9499f5b8d45d933ce62cf24a2995acbe16a70a4
BlaZed <54780693+Fire-Coding@users.noreply.github.com>
2020-09-05 18:00:24 -0400

    Updated bot.js, released UserInfoBot v2

--- bot.js
+++ bot.js
@@ -1,17 +1,15 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
 
-var token = 'token';
-
 const PREFIX = "-";
 
 var version = "1.0.0"
 
 bot.on('ready', () =>{
     console.log('User Info Bot is online!')
-    //bot.user.setActivity("User Info Bot v2 is now out!", {type: "PLAYING"})
+    bot.user.setActivity("User Info Bot v2 is now out!", {type: "PLAYING"})
 
-    let statuses = [
+    /*let statuses = [
         `${bot.guilds.size} Servers!`,
         `${bot.users.size} People!`,
         '-help'
@@ -20,7 +18,7 @@ bot.on('ready', () =>{
     setInterval(function() {
         let status = statuses[Math.floor(Math.random() * statuses.length)];
         bot.user.setActivity(status, {type: "WATCHING"});
-    }, 5000)
+    }, 5000)*/
 })
 
 
@@ -35,58 +33,57 @@ bot.on('message', message=>{
         break;
 
         case 'userinfo':
-            var embed = new Discord.RichEmbed()
+            var embed = new Discord.MessageEmbed()
             .setTitle('User Information')
             .addField('Player Name', message.author.username)
             .addField('Server Sent From', message.guild.name)
             .setColor(0xF92C00)
             .setThumbnail(message.author.avatarURL)
-            message.channel.sendEmbed(embed);
+            message.channel.send(embed);
         break;
 
         case 'help':
-            var embed = new Discord.RichEmbed()
+            var embed = new Discord.MessageEmbed()
             .setTitle('Commands')
-            .addField('Commands List', '-commands')
+            .addField('Commands List', '-help')
             .addField('Pong!', '-ping')
             .addField('Gives Info on You!', `-userinfo`)
             .addField('Gives Info on the Bot!', '-botinfo')
-            .addField('Clears Messages', '-clear')
-            .addField('Owner Info', '-ownerinfo')
+            .addField('Developer Info', '-devinfo')
             .addField('Invite Bot', '-invite')
             .addField('Discord', '-discord')
             .setColor(0xF92C00)
-            message.channel.sendEmbed(embed);
+            message.channel.send(embed);
         break;
 
         case 'botinfo':
-            var embed = new Discord.RichEmbed()
+            var embed = new Discord.MessageEmbed()
             .setTitle('Bot Info')
             .addField('Version', version)
             .addField('Lines of Code', '64')
             .addField('Developer', 'BlaZed')
             .setColor(0xF92C00)
-            message.channel.sendEmbed(embed);
+            message.channel.send(embed);
         break;
 
         case 'ownerinfo':
-            var embed = new Discord.RichEmbed()
+            var embed = new Discord.MessageEmbed()
             .setTitle('Owner Info')
             .addField('Online Name', 'Overblown')
             .addField('Mixer', 'BlaZedPlayz')
             .addField('Coding Languages He Knows', 'JavaScript, HTML, CSS')
             .setColor(0xF92C00)
-            message.channel.sendEmbed(embed);
+            message.channel.send(embed);
         break;
 
         case 'invite':
-            message.channel.sendMessage('Invite:')
-            message.channel.sendMessage('https://discordapp.com/api/oauth2/authorize?client_id=658860706766127104&permissions=8&scope=bot')
+            message.channel.send('Invite:')
+            message.channel.send('https://discord.com/api/oauth2/authorize?client_id=751912754289573959&permissions=8&scope=bot')
         break;
 
         case 'discord':
-            message.channel.sendMessage('Discord:')
-            message.channel.sendMessage('https://discord.gg/GebHzZB')
+            message.channel.send('Discord:')
+            message.channel.send('https://discord.gg/BCukkMX')
         break;
     }
 })
